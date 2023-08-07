fa13444642e53cbb679858a0f3d901a63bc03842
Tyler <t.woods95@aol.com>
2023-08-07 18:30:43 -0400

    updated webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -62,7 +63,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              ['@babel/plugin-proposal-object-rest-spread'],
+              // ['@babel/plugin-proposal-object-rest-spread'],
             },
           },
         },
